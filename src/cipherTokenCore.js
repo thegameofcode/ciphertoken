f92757498d3c5a592f66db664c5660706adbe2b6
Kristina Lyubomirova <kristina.lyubomirova@bbva.com>
2014-11-13 01:41:34 +0100

    access token get set - happy path is done

--- src/cipherTokenCore.js
+++ src/cipherTokenCore.js
@@ -38,6 +38,20 @@ function standarizeToken(token){
         ;
 }
 
+function unserialize(data) {
+    try {
+        return JSON.parse(data);
+    } catch (e) {
+        throw _ERRORS.unserialization_error;
+    }
+}
+
+function firmAccessToken(settings, userId, timestamp, serializedData) {
+    var firmedToken = crypto.createHmac(settings.hmacAlgorithm, settings.firmKey)
+        .update(userId + timestamp + serializedData)
+        .digest(settings.hmacDigestEncoding);
+    return firmedToken;
+}
 exports.createAccessToken = function(settings, userId, timestamp, data) {
     for (var p in DEFAULT_SETTINGS) settings[p] = DEFAULT_SETTINGS[p];
 
@@ -45,13 +59,28 @@ exports.createAccessToken = function(settings, userId, timestamp, data) {
     data = data || {};
     var serializedData = serialize(data);
 
-    var firmedToken = crypto.createHmac(settings.hmacAlgorithm, settings.firmKey)
-        .update(userId + timestamp + serializedData)
-        .digest(settings.hmacDigestEncoding);
+    var firmedToken = firmAccessToken(settings, userId, timestamp, serializedData);
 
     var cipher = crypto.createCipher(settings.cipherAlgorithm, settings.cipherKey);
     var accessTokenSet = [userId, timestamp, serializedData, firmedToken];
     var encodedData = cipher.update(accessTokenSet.join(SEPARATOR), settings.plainEncoding, settings.tokenEncoding);
 
     return  standarizeToken(encodedData + cipher.final(settings.tokenEncoding));
+};
+
+exports.getAccessTokenSet = function(settings, accessToken){
+    var tokenSet = {};
+
+    var decipher = crypto.createDecipher(settings.cipherAlgorithm, settings.cipherKey);
+
+
+    var token = decipher.update(accessToken, settings.tokenEncoding, settings.plainEncoding);
+    token =  (token + decipher.final(settings.plainEncoding)).split(SEPARATOR);
+    token[2] = unserialize(token[2]);
+
+    console.log(token);
+
+    tokenSet = { userId : token[0], timestamp : token[1], data: token[2]};
+
+    return tokenSet;
 };
--- src/test/cipherTokenCoreTest.js
+++ src/test/cipherTokenCoreTest.js
@@ -1,18 +1,31 @@
 var assert = require('assert');
 var ctCore = require('../cipherTokenCore');
 
-describe('Access token generation', function(){
+const USER_ID = 'userId';
+const DATA = 'validData';
+
+var config = {
+    cipherKey: 'myCipherKey123',
+    firmKey:  'myFirmKey123'
+};
+
+describe.only('Access token generation', function(){
 
     it('Should generate access token', function(){
-        var config = {
-            cipherKey: 'myCipherKey123',
-            firmKey:  'myFirmKey123'
-        };
-
-        var accessToken = ctCore.createAccessToken(config, 'userId', new Date().getTime(), 'validData');
-
+        var accessToken = ctCore.createAccessToken(config, USER_ID, new Date().getTime(), DATA);
         assert.notEqual(accessToken, null);
-    })
+    });
+
+    it('Generated access token must be decoded back to get original data', function(){
+        var timestamp = new Date().getTime();
+        var accessToken = ctCore.createAccessToken(config, USER_ID, timestamp, DATA);
+        var accessTokenSet = ctCore.getAccessTokenSet(config, accessToken);
+
+        assert.notEqual(accessTokenSet, null);
+        assert.equal(accessTokenSet.userId, USER_ID);
+        assert.equal(accessTokenSet.timestamp, timestamp);
+        assert.deepEqual(accessTokenSet.data, DATA);
+    });
 
 });
 
