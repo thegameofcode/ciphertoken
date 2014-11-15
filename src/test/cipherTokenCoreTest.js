e860f7f3fdbfb31c662be6b501a1f4d1b2a541d7
Kristina Lyubomirova <kristina.lyubomirova@bbva.com>
2014-11-15 11:01:27 +0100

    removed no needed timestamps from error tests

--- src/test/cipherTokenCoreTest.js
+++ src/test/cipherTokenCoreTest.js
@@ -17,7 +17,7 @@ var anotherSettings = {
 describe('Access token generation', function(){
 
     it('Should generate access token', function(){
-        var accessToken = ctCore.createAccessToken(settings, USER_ID, new Date().getTime(), DATA);
+        var accessToken = ctCore.createAccessToken(settings, USER_ID, DATA);
         assert.notEqual(accessToken, null);
     });
 
@@ -66,7 +66,7 @@ describe('Access token generation', function(){
     });
 });
 
-describe('Error description', function () {
+describe.only('Error description', function () {
 
     it('Should return an error when submitted access token is invalid', function() {
         var accessToken = 'invalid access token';
@@ -78,7 +78,7 @@ describe('Error description', function () {
     });
 
     it('Should return an error when trying to decode with invalid firm key', function(){
-        var accessToken = ctCore.createAccessToken(settings, USER_ID, new Date().getTime(), DATA);
+        var accessToken = ctCore.createAccessToken(settings, USER_ID, DATA);
         var accessTokenSet = ctCore.getAccessTokenSet(anotherSettings, accessToken);
 
         assert.notEqual(accessTokenSet, null);
@@ -88,7 +88,7 @@ describe('Error description', function () {
 
     it('Should throw an error when trying to create an accessToken with empty settings', function () {
         try {
-            ctCore.createAccessToken({}, USER_ID, new Date().getTime(), DATA);
+            ctCore.createAccessToken({}, USER_ID, DATA);
         } catch(err) {
             assert.notEqual(err, null);
             assert.strictEqual(err.err, 'Settings required');
@@ -97,7 +97,7 @@ describe('Error description', function () {
 
     it('Should throw an error when trying to create an accessToken with undefined settings', function () {
         try {
-            ctCore.createAccessToken(undefined, USER_ID, new Date().getTime(), DATA);
+            ctCore.createAccessToken(undefined, USER_ID, DATA);
         } catch(err) {
             assert.notEqual(err, null);
             assert.strictEqual(err.err, 'Settings required');
@@ -106,7 +106,7 @@ describe('Error description', function () {
 
     it('Should throw an error when cipherKey is missing', function () {
         try {
-           ctCore.createAccessToken({'firmKey': 'firmKey1234'}, USER_ID, new Date().getTime(), DATA);
+           ctCore.createAccessToken({'firmKey': 'firmKey1234'}, USER_ID, DATA);
         } catch(err) {
            assert.notEqual(err, null);
            assert.strictEqual(err.err, 'CipherKey required');
@@ -115,10 +115,10 @@ describe('Error description', function () {
 
     it('Should throw an error when firmKey is missing', function () {
         try {
-            ctCore.createAccessToken({'cipherKey': 'cipherKey1234'}, USER_ID, new Date().getTime(), DATA);
+            ctCore.createAccessToken({'cipherKey': 'cipherKey1234'}, USER_ID, DATA);
         } catch(err) {
             assert.notEqual(err, null);
             assert.strictEqual(err.err, 'FirmKey required');
         }
-    })
+    });
 });
